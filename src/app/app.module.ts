dff3e0479da70aefdead5f882a6458db35f1f34f
glen <glen.mboroki@student.moringaschool.com>
2022-02-23 08:12:31 +0300

    src

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { ProfileService } from './services/profile.service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    NavbarComponent, 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+   
+  ],
+  providers: [ProfileService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
